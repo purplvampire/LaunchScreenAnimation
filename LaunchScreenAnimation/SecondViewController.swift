9343e4450339fedbdd3c94757d25137890cd3d7c
Jason Chen <purplvampire@gmail.com>
2024-10-02 22:54:55 +0800

    Create Launch VC

---
+++ LaunchScreenAnimation/SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  LaunchScreenAnimation
+//
+//  Created by 陳信彰 on 2024/10/2.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
