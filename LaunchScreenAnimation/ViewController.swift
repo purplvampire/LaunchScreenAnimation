474b8ac7e34c1b19fdd861ef85545c469f6c7594
Jason Chen <purplvampire@gmail.com>
2024-10-02 21:35:11 +0800

    Initial Commit

---
+++ LaunchScreenAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LaunchScreenAnimation
+//
+//  Created by 陳信彰 on 2024/10/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
